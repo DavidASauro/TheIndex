bbb8787aae1bf6cf20d49ac937fc2929b88151d9
DavidASauro <DavidSau43@gmail.com>
2026-03-08 12:34:03 -0400

    added general page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 const page = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="text-8xl">The Index</h1>
+        <p className="text-2xl">
+          A place where I can show all the fun things I made.
+        </p>
+      </div>
+
+      <div>{/*Content Goes Here*/}</div>
+    </div>
+  );
 };
 
 export default page;
