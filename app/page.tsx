270ed1b47b683a60b6179f2734357656ee3fbcee
DavidASauro <DavidSau43@gmail.com>
2026-03-12 17:34:50 -0400

    fixed font sizing issue on smaller screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div className="w-full mx-auto flex flex-col gap-20 px-5">
       <div className="text-center mt-10 flex flex-col gap-5">
-        <h1 className="text-8xl text-[#805428] font-fraunces font-semibold italic [text-shadow:3px_2px_3px_#3E2723] tracking-wide">
+        <h1 className="text-7xl  lg:text-8xl text-[#805428] font-fraunces font-semibold italic [text-shadow:3px_2px_3px_#3E2723] tracking-wide">
           {`David's Pinboard`}
         </h1>
         <p className="text-2xl text-[#805428] font-nunito font-bold">
